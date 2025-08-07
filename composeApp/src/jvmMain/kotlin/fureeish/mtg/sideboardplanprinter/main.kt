6e4244938442cffcfb26f3a93e5ec50cf19b7139
Fureeish <f74.kwiatek@gmail.com>
2025-08-08 00:50:05 +0200

    Make title dependent on a resource

--- composeApp/src/jvmMain/kotlin/fureeish/mtg/sideboardplanprinter/main.kt
+++ composeApp/src/jvmMain/kotlin/fureeish/mtg/sideboardplanprinter/main.kt
@@ -2,11 +2,14 @@ package fureeish.mtg.sideboardplanprinter
 
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import mtg_sideboard_plan_printer.composeapp.generated.resources.Res
+import mtg_sideboard_plan_printer.composeapp.generated.resources.app_name
+import org.jetbrains.compose.resources.stringResource
 
 fun main() = application {
     Window(
         onCloseRequest = ::exitApplication,
-        title = "MTG-Sideboard-Plan-Printer",
+        title = stringResource(Res.string.app_name),
     ) {
         App()
     }
