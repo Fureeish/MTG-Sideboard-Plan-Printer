75fb422026711f8faf373af56e4b990a79df73c5
Fureeish <f74.kwiatek@gmail.com>
2025-08-08 00:49:53 +0200

    Add Kodein and navigation dependencies

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -20,6 +20,8 @@ kotlin {
             implementation(compose.components.uiToolingPreview)
             implementation(libs.androidx.lifecycle.viewmodelCompose)
             implementation(libs.androidx.lifecycle.runtimeCompose)
+            implementation(libs.kodein.di)
+            implementation(libs.compose.navigation)
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
