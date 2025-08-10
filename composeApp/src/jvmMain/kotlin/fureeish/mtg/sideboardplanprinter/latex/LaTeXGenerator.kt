70048a4ffa46aa28358d47766ffbf5ab16aa7352
Fureeish <f74.kwiatek@gmail.com>
2025-08-10 03:32:16 +0200

    feat: Replace configuration for PDF generation from a simple set of options to DSL builder
    feat: Support "Full15" layout for sideboard matchups

--- composeApp/src/jvmMain/kotlin/fureeish/mtg/sideboardplanprinter/latex/LaTeXGenerator.kt
+++ composeApp/src/jvmMain/kotlin/fureeish/mtg/sideboardplanprinter/latex/LaTeXGenerator.kt
@@ -1,23 +1,57 @@
 package fureeish.mtg.sideboardplanprinter.latex
 
+import fureeish.mtg.sideboardplanprinter.latex.LaTeXGenerator.Config.Layout
 import fureeish.mtg.sideboardplanprinter.mtg.Card
+import fureeish.mtg.sideboardplanprinter.mtg.SideboardPlan
 
 object LaTeXGenerator {
+    class Config private constructor() {
+        companion object {
+            operator fun invoke(builder: Config.() -> Unit) = Config().apply(builder)
+            val default: Config
+                get() = Config()
+        }
+
+        var withInOut: Boolean = true
+        var alignment: AlignTo = AlignTo.Left
+        var layout: Layout = Layout.ClassicInOut
+
+        sealed class AlignTo {
+            object Center : AlignTo()
+            object Left : AlignTo()
+        }
+
+        sealed class Layout {
+            object ClassicInOut : Layout()
+            object Full15 : Layout()
+        }
+    }
+
     private val preamble = """
         \documentclass[9pt]{extarticle}
         \usepackage[lining]{ebgaramond}
         \usepackage[margin=0.75in]{geometry}
         \usepackage{array}
+        \usepackage{microtype}
     """.trimIndent()
 
     fun generateLaTeXFrom(
-        matchups: Map<String, List<Card>>,
-        columnCount: Int,
-        options: Iterable<Options> = setOf(Options.AlignToLeft)
+        sideboardPlan: SideboardPlan,
+        columns: Int,
+        config: Config = Config.default
     ): String {
-        val matchupsSortedByPlanItems = matchups
-            .map { (matchup, sideboardPlan) ->
-                val (cardsToAdd, cardsToRemove) = sideboardPlan.partition { it.count > 0 }
+        val matchupsSortedByPlanSizes = sideboardPlan.matchupPlans
+            .map { (matchup, sideboardForMatchup) ->
+                var (cardsToAdd, cardsToRemove) = sideboardForMatchup.partition { it.count > 0 }
+
+                if (config.layout == Layout.Full15) {
+                    val fakeSideboardedCards = sideboardPlan.sideboard - cardsToAdd.toSet()
+                    cardsToRemove += fakeSideboardedCards.map { it.copy(count = -it.count) }
+                    cardsToRemove = cardsToRemove.groupingBy { it.cardName }
+                        .reduce { _, acc, newCard -> Card(acc.count + newCard.count, acc.cardName) }
+                        .values
+                        .toList()
+                }
 
                 val inOutPlan = zipWithPadding(
                     cardsToAdd.sortedByDescending(Card::count),
@@ -33,33 +67,35 @@ object LaTeXGenerator {
                 inoutPlan.size
             }
 
-        val latexRepresentation = matchupsSortedByPlanItems
-            .chunked(columnCount)
+        val latexRepresentation = matchupsSortedByPlanSizes
+            .chunked(columns)
             .joinToString(
                 prefix = "\\noindent\n",
                 separator = "\n\\vspace{1em}\n\n\\noindent\n"
             ) { matchupsRow ->
                 matchupsRow
-                    .joinToString(separator = "\\hfill\n") { (matchup, sideboardPlan) ->
-                        val singlePlanWidth = String.format("%.3f", 1.0 / columnCount / 1.05)
+                    .joinToString(separator = "\\hfill\n") { (matchup, sideboardForMatchup) ->
+                        val singlePlanWidth = String.format("%.3f", 1.0 / columns / 1.05)
 
                         buildString {
                             append("\\begin{minipage}[t]{$singlePlanWidth\\textwidth}\n")
                             append("\\centering\n")
                             append("\\textbf{\\textsc{$matchup}}\\vphantom{Jy} \\\\\n")
 
-                            val leftColumnAlignment =
-                                if (options.contains(Options.AlignToLeft)) "\\raggedright" else "\\raggedleft"
+                            val leftColumnAlignment = when (config.alignment) {
+                                Config.AlignTo.Left -> "\\raggedright"
+                                Config.AlignTo.Center -> "\\raggedleft"
+                            }
 
                             append("\\begin{tabular}{@{}>{$leftColumnAlignment\\arraybackslash}p{0.45\\linewidth}|>{\\raggedright\\arraybackslash}p{0.45\\linewidth}@{}}")
 
-                            if (options.contains(Options.WithInOut)) {
-                                append("\\textbf{\\textsc{in}} & \\textbf{\\textsc{out}} \\\\\n")
+                            if (config.withInOut) {
+                                append("\\hfill\\textbf{\\textsc{in}} & \\textbf{\\textsc{out}} \\\\\n")
                             }
 
                             append("\\hline\n")
 
-                            sideboardPlan.forEach { (`in`, out) ->
+                            sideboardForMatchup.forEach { (`in`, out) ->
                                 append("$`in` & $out\\\\\n")
                             }
 
@@ -87,9 +123,5 @@ object LaTeXGenerator {
             val second = list2.getOrNull(i)
             first to second
         }
-    }
-
-    enum class Options {
-        WithInOut, AlignToCenter, AlignToLeft
     }
 }
